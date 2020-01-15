bdf502db63cea5a1a5c1acc7c7ec52571292b04f
niki-ai <techbins@niki.ai>
2020-01-15 16:43:24 +0530

    updating API code

--- main.py
+++ main.py
@@ -4,15 +4,35 @@ from flair.models import TextClassifier
 from flair.data import Sentence
 classifier = TextClassifier.load('./model/best-model.pt')
 
+mapping = {
+    'sad': '&#x1F61E',
+    'smile': '&#x1F600',
+    'food': '&#x1F37D',
+    'heart': '&#10084;',
+    'baseball': '&#x26be;'
+}
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/emojify', methods=['POST'])
 def emoji():
         data = request.form.get('text')
+        if not len(data.strip()):
+            return ''
         sentence = Sentence(data)
         classifier.predict(sentence)
-        return str(sentence.labels)
+        print (str(sentence.labels))
+        if 'sad'in str(sentence.labels):
+            return mapping['sad'] 
+        elif 'smile' in str(sentence.labels):
+            return mapping['smile'] 
+        elif 'food' in str(sentence.labels):
+            return mapping['food']
+        elif 'heart' in str(sentence.labels):
+            print (1)
+            return mapping['heart']
+        elif 'baseball' in str(sentence.labels):
+            return mapping['baseball']
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
